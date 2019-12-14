8e5cd5c2abd7488c3bdc1c826fdc0c351ed1a33b
Kelsey Hightower <kelsey.hightower@gmail.com>
2019-12-14 15:05:43 -0800

    add secrets support

--- secrets.go
+++ secrets.go
@@ -26,7 +26,19 @@ func formatSecretVersion(project, name, version string) string {
 	return fmt.Sprintf("projects/%s/secrets/%s/versions/%s", project, name, version)
 }
 
+func AccessSecretVersion(name, version string) (string, error) {
+	return accessSecretVersion(name, version)
+}
+
 func AccessSecret(name string) (string, error) {
+	return accessSecretVersion(name, "latest")
+}
+
+func accessSecretVersion(name, version string) (string, error) {
+	if version == "" {
+		version = "latest"
+	}
+
 	token, err := Token([]string{"https://www.googleapis.com/auth/cloud-platform"})
 	if err != nil {
 		return "", err
@@ -37,7 +49,7 @@ func AccessSecret(name string) (string, error) {
 		return "", err
 	}
 
-	secretVersion := formatSecretVersion(numericProjectID, name, "latest")
+	secretVersion := formatSecretVersion(numericProjectID, name, version)
 	endpoint := fmt.Sprintf("%s/%s:access", secretmanagerEndpoint, secretVersion)
 
 	request, err := http.NewRequest("GET", endpoint, nil)
